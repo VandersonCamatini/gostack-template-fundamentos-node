1af4cecfcf160d7dc467b24532d69104a28812cf
Vanderson Teodoro camatini <vandersoncamatini66@gmail.com>
2020-08-29 14:08:26 -0300

    Desafio completo. SOLID API com nodeJS e TS.

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestTDO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,25 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestTDO): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw Error(
+        'The informed type is invalid. Expected is incorme or outcome.',
+      );
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw Error('You do not have enough balance.');
+    }
+    const createdTransaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return createdTransaction;
   }
 }
 
